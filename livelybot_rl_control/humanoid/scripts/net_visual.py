9bb3eadda904a9148141e25f3b3ee746bec353a1
HBUTHUANGPX <2082672018@qq.com>
2024-07-15 22:19:35 +0800

    himloco_test_2

---
+++ livelybot_rl_control/humanoid/scripts/net_visual.py
@@ -0,0 +1,10 @@
+import torch
+import netron
+import torch
+from torch import nn
+from torchinfo import summary
+
+policy = torch.jit.load("/home/hpx/HPXLoco/livelybot_rl_control/logs/Pai_ppo/exported/policies/policy.pt")
+# policy  = torch.load('/home/hpx/HPXLoco/livelybot_rl_control/logs/Pai_ppo/Jul14_19-19-06_v1/model_0.pt')
+
+print(policy)
